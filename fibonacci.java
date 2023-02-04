399b45925f9648350567aee80212d3c199d1320b
Tejasnamikaze <95167495+Tejasnamikaze@users.noreply.github.com>
2023-02-04 23:55:12 +0530

    commit 2

--- fibonacci.java
+++
@@ -1,9 +0,0 @@
-class fibonacci {
-    public static void main(String args[]) {
-
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world *3");
-
-    }
-}
--- first.py
+++
@@ -1,2 +0,0 @@
-c = "tejas"
-print(c+" hey aditi")
